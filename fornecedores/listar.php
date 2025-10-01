ed5d23ca52c2879f11271c1d7a1f89beaf44b556
Amorim_dev <guilhermeamorimrochalima@gmail.com>
2025-10-01 16:28:51 -0300

    feat(fornecedores): adicionar link e arquivo para exclusão de fornecedor
    - Incluído link dinâmico para excluir.php na lista de fornecedores no arquivo fornecedores/lista.php
    - Criado arquivo excluir.php para processar a exclusão de registro pelo id

--- fornecedores/listar.php
+++ fornecedores/listar.php
@@ -51,7 +51,7 @@ echo "<pre>";
                 <td>
                     <!-- LINK DINÂMICO, ou seja, a url/endereço utiliza parâmetro(s) e valor(es) dinâmico(s) -->
                     <a href="editar.php?id=<?=$fornecedor["id"]?>">📝Editar</a>
-                    <a href="">❌Excluir</a>
+                    <a href="excluir.php?id=<?=$fornecedor["id"]?>">❌Excluir</a>
 				</td>
             </tr> 
 
