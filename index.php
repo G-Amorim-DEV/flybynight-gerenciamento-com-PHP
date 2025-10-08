757e221276e0a145488f948308eef88305acd9dd
Amorim_dev <guilhermeamorimrochalima@gmail.com>
2025-10-08 17:27:26 -0300

    style(css): adicionar justify-self na classe .subtitulo para alinhamento
    Essa alteração define o alinhamento da classe .subtitulo para o início do container de grid, melhorando o posicionamento visual do subtítulo.

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
     <link rel="stylesheet" href="css/estilos.css">
 </head>
 <body>
-    <h1>Fly By Night - CRUD com PHP e SQL</h1>
-    <h2>Gerenciar</h2>
+    <h1 class="titulo">Fly By Night - CRUD com PHP e SQL</h1>
+    <h2 class="subtitulo">Gerenciar</h2>
     <ul>
         <li><a href="fornecedores/listar.php">Fornecedores</a></li>
         <li><a href="produtos/listar.php">Produtos</a></li>
