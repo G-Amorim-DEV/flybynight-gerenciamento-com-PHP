e5d5ff4059166d6df88a3649612c49a3e8b0a6e3
Amorim_dev <guilhermeamorimrochalima@gmail.com>
2025-10-09 14:32:57 -0300

    feat: Adiciona função para buscar todos os produtos com nome do fornecedor
    Esta função `buscarProdutos` realiza uma consulta SQL que retorna o ID, nome e quantidade de todos os produtos, além de incluir o nome do fornecedor associado.
    
    - Realiza um `JOIN` entre as tabelas `produtos` e `fornecedores` usando `fornecedor_id`.
    - Ordena os resultados pelo nome do produto.
    - A função está implementada em `src/produto_crud.php`.

--- src/produto_crud.php
+++ src/produto_crud.php
@@ -3,9 +3,16 @@
 // src/produto_crud.php
 
 
-require_once "../conecta.php"
+require_once "../conecta.php";
 
-function buscarProdutos($conexao){
-    
-}
+    function buscarProdutos($conexao) {
+        $sql = "SELECT produtos.id, produtos.nome, produtos.quantidade, fornecedores.nome
+            FROM produtos
+            JOIN fornecedores ON fornecedores.id = produtos.fornecedor_id
+            ORDER BY produtos.nome";
+
+        $consulta = $conexao->query($sql);
+
+            return $consulta->fetchAll();
+    }
 ?>
